6547a389250466d63dee5db88d425352909a8eed
oliver <myungsekyo@gmail.com>
2018-08-25 20:27:28 +0900

    added level, problem models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse_lazy
 
-# Create your models here.
+
+class Level(models.Model):
+    title = models.CharField('title', max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy('app:level_detail', kwargs={'pk': self.id})
+
+
+class Problem(models.Model):
+    title = models.CharField('title', max_length=50)
+    level = models.ForeignKey('app.Level', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
