a7aab9d89c6ee5be34799ba1b3e6940326d0d04d
oliver <myungsekyo@gmail.com>
2018-08-25 20:47:41 +0900

    added urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
-from .views import IndexView
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', HomeView.as_view(), name='home'),
+    path('levels/', LevelListView.as_view(), name='level_list'),
+    path('levels/<int:pk>/', LevelDetailView.as_view(), name='level_detail'),
+    path('speech_to_text/', SpeechToTextView.as_view(), name='stt'),
+    path('api/stt/', get_text_from_speech, name='api-stt'),
+    path('api/tts/', get_speech_from_text, name='api-tts'),
 ]
