37eb15d655e477cfb1a1e92e019c89e4eb76524a
oliver <myungsekyo@gmail.com>
2018-09-02 23:39:41 +0900

    update model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,10 @@ from django.urls import reverse_lazy
 
 
 class Level(models.Model):
-    title = models.CharField('title', max_length=50)
+    title = models.CharField('제목', max_length=50)
+    content = models.TextField('내용')
+    made_by_user = models.BooleanField('made_by_user', default=False)
+    created = models.DateTimeField('created', auto_now=True)
 
     def __str__(self):
         return self.title
@@ -13,7 +16,7 @@ class Level(models.Model):
 
 
 class Problem(models.Model):
-    title = models.CharField('title', max_length=50)
+    title = models.CharField('문제', max_length=50)
     level = models.ForeignKey('app.Level', on_delete=models.CASCADE)
 
     def __str__(self):
