fbe8b3538973a671163057e0f60a79d138a93db2
oliver <myungsekyo@gmail.com>
2018-09-02 23:39:33 +0900

    update url

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,7 @@ from .views import *
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('levels/', LevelListView.as_view(), name='level_list'),
+    path('levels/create/', LevelFormView.as_view(), name='level_create'),
     path('levels/<int:pk>/', LevelDetailView.as_view(), name='level_detail'),
-    path('speech_to_text/', SpeechToTextView.as_view(), name='stt'),
-    path('api/stt/', get_text_from_speech, name='api-stt'),
     path('api/tts/', get_speech_from_text, name='api-tts'),
 ]
