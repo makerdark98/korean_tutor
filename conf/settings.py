03b80f4c636e77240a387f23599b6aeb0900058d
Min-Uk.Lee <makerdark98@naver.com>
2018-09-06 23:51:13 +0900

    Add files for deployment to paas-ta

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['korean_tutor.paas-ta.co.kr', '*']
 
 
 # Application definition
